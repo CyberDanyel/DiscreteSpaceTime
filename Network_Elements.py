a37e2e01d83a90353c4306204a4adc15607a2696
GangaSM <115886383+GangaSM@users.noreply.github.com>
2023-01-13 09:38:37 +0000

    Update Network_Elements.py

--- Network_Elements.py
+++ Network_Elements.py
@@ -25,24 +25,24 @@ class Node:
     Methods
     -------
     pos : returns position
-    get_patch : returns patch
+    patch : returns either a large(vertex) or small(node) patch
     
     '''
     
     def __init__(self,position=np.array([0.0,0.0]),vertex=False):
-        self._r = position
-        self._vertex = vertex
-        self._patch1 = plt.Circle(self._r,0.01,fc='white')
-        self._patch2 = plt.Circle(self._r,0.05,fc='red')
+        self.r = position
+        self.vertex = vertex
+        self.nodepatch = plt.Circle(self.r,0.01,fc='white')
+        self.vertexpatch = plt.Circle(self.r,0.05,fc='white')
 
     def pos(self):
-        return self._r
+        return self.r
 
-    def get_patch(self):
-        if self._vertex == False:
-            return self._patch1
+    def patch(self):
+        if self.vertex == False:
+            return self.nodepatch
         else:
-            return self._patch2
+            return self.vertexpatch
     
 #%%
 
@@ -50,18 +50,42 @@ class Edge:
     
     '''
     
-    Docstring
+    This class defines the edge object.
+    
+    Attributes
+    ----------
+    start : numpy array
+            position of source node
+    end : numpy array
+          position of sink node
+    dist : float
+           distance between start and end or edge weight
+    
+    Methods
+    -------
+    dist : returns edge weight
+    edge_patch : returns default edge patch
+    path_patch : return special path patch
     
     '''
     
     def __init__(self,start=Node(position=np.array([0.0,0.0])),
-                      end=Node(position=np.array([0.0,0.0])),diag=False):
-        self._start = start.pos()
-        self._end = end.pos()
-        self._diag = diag
+                      end=Node(position=np.array([0.0,0.0]))):
+        self.start = start.pos()
+        self.end = end.pos()
 
-        style = ArrowStyle("Fancy", head_length=5, head_width=5, tail_width=0.5)
-        self._patch = FancyArrowPatch(self._start,self._end,arrowstyle=style,color='purple')
+        style = ArrowStyle("Fancy",head_length=5,head_width=5,tail_width=0.5)
+        self.edgepatch = FancyArrowPatch(self.start,self.end,arrowstyle=style,color='dodgerblue')
+        self.pathpatch = FancyArrowPatch(self.start,self.end,arrowstyle=style,color='magenta')
         
-    def get_patch(self):
-            return self._patch
+    def dist(self):
+        self.distance = np.linalg.norm(self.start-self.end)
+        return self.distance
+        
+    def edge_patch(self):
+        return self.edgepatch
+    
+    def path_patch(self):
+        return self.pathpatch
+    
+#%%
