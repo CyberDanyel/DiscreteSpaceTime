88c8e6370bd0299e9dc0afa7668788fed80d3198
GangaSM <ganga.transformers@gmail.com>
2023-03-01 21:22:21 +0000

    commit

--- DAG.py
+++ DAG.py
@@ -8,24 +8,20 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.transforms import Affine2D
-import mpl_toolkits.axisartist.floating_axes as floating_axes
-from collections import *
 from matplotlib.patches import FancyArrowPatch, ArrowStyle
-from copy import deepcopy
+from collections import *
+
 #%%
     
 class DAG:
     
     '''
     To Do:
-        - Plot shortest, longest and geodesic on the same graph
         - Reduce the DAG to the interval
         - Carry out investigations for varying p
         - Implement CHI2 measure of path deviation from geodesic
-        - Fix plotting of space-time events
     '''
-    def __init__(self,N,show=False):
+    def __init__(self,N):
         # Initialise dictionary structures
         self.N = N
         self.nodes = defaultdict(list)
@@ -38,7 +34,7 @@ class DAG:
         self.shortestnodespdic = [defaultdict(list) for i in range(self.N)]
         self.longpaths = defaultdict(list)
         self.longestnodespdic = [defaultdict(list) for i in range(self.N)]
-        np.random.seed(2)
+        np.random.seed(1)
         
         # Generate random nodes
         self.t,self.s = [],[]
@@ -97,26 +93,6 @@ class DAG:
         #     else:
         #         continue
         # self.N = len(self.nodes)
-        
-        if show == True:
-            with plt.style.context('ggplot'):
-                fig = plt.figure(dpi=540)
-                tr = Affine2D().scale(1,1).rotate_deg(45)
-                grid_helper = floating_axes.GridHelperCurveLinear(tr,extremes=(0,1,0,1))
-                ax = floating_axes.FloatingSubplot(fig,111,grid_helper=grid_helper)
-                aux_ax = ax.get_aux_axes(tr)
-                fig.add_subplot(ax)
-                ax.grid(False)
-                ax.set_xlabel(r'$ct$')
-                ax.set_ylabel(r'$x$')
-                aux_ax.plot(self.t,self.s,'.',color='magenta')
-                n = np.linspace(0,1,10)
-                aux_ax.plot(n,n,color='dodgerblue')
-                for i in range(self.N-1):
-                    for j in range(len(self.adj[i])):
-                        aux_ax.arrow(self.nodes[i][0],self.nodes[i][1],self.nodes[self.adj[i][j][0]][0]-self.nodes[i][0],self.nodes[self.adj[i][j][0]][1]-self.nodes[i][1],length_includes_head = True, color = 'red')
-                #ax.legend()
-                plt.show()
                 
     def minkowski(self,ps):
         for i in range(len(ps)):
@@ -130,17 +106,15 @@ class DAG:
     def tSort(self,v,visited,stack):        
         # Mark current node as visited
         visited[v] = True
- 
         # Recur for all nodes adjacent to this node
         if v in self.adj.keys():
             for node,weight in self.adj[v]:
                 if visited[node] == False:
                     self.tSort(node,visited,stack)
- 
         # Push current node to stack which stores topological sort
         stack.append(v)
 
-    def short(self, findpath = False, displayps = None): 
+    def short(self,findpath=False,ps=None): 
         # Initialize distances to all nodes as infinite and
         # distance to source as 0
         for p in self.adj[0][0][1].keys():
@@ -151,28 +125,21 @@ class DAG:
             for i in range(self.N):
                 if visited[i] == False:
                     self.tSort(0,visited,stack)
-
             dist = [float("Inf")] * (self.N)
             dist[0] = 0
- 
             # Process nodes in topological order
-        
             while stack:
- 
                 # Get the next node from topological order
                 i = stack.pop()
- 
                 # Update distances of all adjacent nodes
                 for node,weights in self.adj[i]:
                     if dist[node] > dist[i] + weights[p]:
                         dist[node] = dist[i] + weights[p]
                         if findpath == True:
-                            self.shortestnodespdic[node][p] = i
-                            
+                            self.shortestnodespdic[node][p] = i            
                 self.shortest_dic[p] = dist[self.N-1]
-                
         if findpath == True:
-            for p in displayps:
+            for p in ps:
                 j = self.N - 1
                 self.shortpaths[p].append(j)
                 while True:
@@ -183,65 +150,35 @@ class DAG:
                         break
                 self.shortpaths[p] = self.shortpaths[p][::-1]
                 
-                
-                with plt.style.context('ggplot'):
-                    fig = plt.figure(dpi=540)
-                    tr = Affine2D().scale(1,1).rotate_deg(45)
-                    grid_helper = floating_axes.GridHelperCurveLinear(tr,extremes=(0,1,0,1))
-                    ax = floating_axes.FloatingSubplot(fig,111,grid_helper=grid_helper)
-                    aux_ax = ax.get_aux_axes(tr)
-                    fig.add_subplot(ax)
-                    ax.grid(False)
-                    ax.set_xlabel(r'$ct$')
-                    ax.set_ylabel(r'$x$')
-                    aux_ax.plot(self.t,self.s,'.',color='magenta')
-                    n = np.linspace(0,1,10)
-                    aux_ax.plot(n,n,color='dodgerblue')
-                    for i in range(self.N-1):
-                        for j in range(len(self.adj[i])):
-                            aux_ax.arrow(self.nodes[i][0],self.nodes[i][1],self.nodes[self.adj[i][j][0]][0]-self.nodes[i][0],self.nodes[self.adj[i][j][0]][1]-self.nodes[i][1],length_includes_head = True, color = 'black')
-                    for i in range(len(self.shortpaths[p])-1):
-                        aux_ax.arrow(self.nodes[self.shortpaths[p][i]][0],self.nodes[self.shortpaths[p][i]][1],self.nodes[self.shortpaths[p][i+1]][0]-self.nodes[self.shortpaths[p][i]][0],self.nodes[self.shortpaths[p][i+1]][1]-self.nodes[self.shortpaths[p][i]][1], length_includes_head = True, color = 'green')
-
-        
-        
-           
-    def long(self, findpath = False, displayps = None):
+    def long(self,findpath=False,ps=None):
         # Mark all nodes as not visited
         for p in self.adj[0][0][1].keys():
             visited = [False]*self.N
             stack =[]
             dist = [-10**9 for i in range(self.N)]
- 
             # Call tSort to store Topological Sort starting from all nodes
             for i in range(self.N):
                 if (visited[i] == False):
                     self.tSort(i,visited,stack)
- 
             # Initialize distances to all nodes as infinite and
             # distance to source as 0
             dist[0] = 0
- 
             # Process nodes in topological order
             while (len(stack) > 0):
-       
                 # Get the next node from topological order
                 u = stack[-1]
                 del stack[-1]
- 
                 # Update distances of all adjacent nodes
                 if (dist[u] != 10**9):
                     for i in self.adj[u]:
                         if (dist[i[0]] < dist[u] + i[1][p]):
                             dist[i[0]] = dist[u] + i[1][p]
                             if findpath == True:
-                                self.longestnodespdic[i[0]][p] = u
-                                
+                                self.longestnodespdic[i[0]][p] = u               
                     self.longest_dic[p] = dist[self.N-1]
             self.longest_dic[p] = dist[self.N-1]
-            
         if findpath == True:
-            for p in displayps:
+            for p in ps:
                 j = self.N - 1
                 self.longpaths[p].append(j)
                 while True:
@@ -252,26 +189,6 @@ class DAG:
                         break
                 self.longpaths[p] = self.longpaths[p][::-1]
                     
-                    
-                with plt.style.context('ggplot'):
-                    fig = plt.figure(dpi=540)
-                    tr = Affine2D().scale(1,1).rotate_deg(45)
-                    grid_helper = floating_axes.GridHelperCurveLinear(tr,extremes=(0,1,0,1))
-                    ax = floating_axes.FloatingSubplot(fig,111,grid_helper=grid_helper)
-                    aux_ax = ax.get_aux_axes(tr)
-                    fig.add_subplot(ax)
-                    ax.grid(False)
-                    ax.set_xlabel(r'$ct$')
-                    ax.set_ylabel(r'$x$')
-                    aux_ax.plot(self.t,self.s,'.',color='magenta')
-                    n = np.linspace(0,1,10)
-                    aux_ax.plot(n,n,color='dodgerblue')
-                    for i in range(self.N-1):
-                        for j in range(len(self.adj[i])):
-                            aux_ax.arrow(self.nodes[i][0],self.nodes[i][1],self.nodes[self.adj[i][j][0]][0]-self.nodes[i][0],self.nodes[self.adj[i][j][0]][1]-self.nodes[i][1],length_includes_head = True, color = 'black')
-                    for i in range(len(self.longpaths[p])-1):
-                        aux_ax.arrow(self.nodes[self.longpaths[p][i]][0],self.nodes[self.longpaths[p][i]][1],self.nodes[self.longpaths[p][i+1]][0]-self.nodes[self.longpaths[p][i]][0],self.nodes[self.longpaths[p][i+1]][1]-self.nodes[self.longpaths[p][i]][1], length_includes_head = True, color = 'red')
-
     def distance_comparison(self):
         self.short()
         keys = list(self.shortest_dic.keys())
@@ -288,35 +205,34 @@ class DAG:
         plt.plot(keys, longest_norm_vals, 'x', color = 'red')
         plt.axvline(x=1, color='r', linestyle='-')
         
-    def show_all(self,ps):
+    def show(self,ps):
         for p in ps:
             with plt.style.context('ggplot'):
-                fig = plt.figure(dpi=540)
-                tr = Affine2D().scale(1,1).rotate_deg(45)
-                grid_helper = floating_axes.GridHelperCurveLinear(tr,extremes=(0,1,0,1))
-                ax = floating_axes.FloatingSubplot(fig,111,grid_helper=grid_helper)
-                aux_ax = ax.get_aux_axes(tr)
-                fig.add_subplot(ax)
-                ax.grid(False)
-                ax.set_xlabel(r'$ct$')
-                ax.set_ylabel(r'$x$')
-                aux_ax.plot(self.t,self.s,'.',color='magenta')
-                n = np.linspace(0,1,10)
-                aux_ax.plot(n,n,color='dodgerblue')
-                for i in range(self.N-1):
-                    for j in range(len(self.adj[i])):
-                        aux_ax.arrow(self.nodes[i][0],self.nodes[i][1],self.nodes[self.adj[i][j][0]][0]-self.nodes[i][0],self.nodes[self.adj[i][j][0]][1]-self.nodes[i][1],length_includes_head = True, color = 'black')
+                plt.figure(dpi=1080)
+                ax = plt.gca()
+                ax.set_aspect('equal',adjustable='box')
+                plt.grid(False)
+                plt.xlabel(r'$x$')
+                plt.ylabel(r'$ct$')
+                plt.xlim(-0.01,1.01)
+                plt.ylim(-0.01,1.01)
+                plt.xticks([0,1])
+                plt.yticks([0,1])
+                plt.plot(self.t,self.s,'.',color='magenta')
+                plt.arrow(0,0,1,1,width=0.005,length_includes_head=True,color='black')
+                #for i in range(self.N-1):
+                    #for j in range(len(self.adj[i])):
+                        #plt.arrow(self.nodes[i][0],self.nodes[i][1],self.nodes[self.adj[i][j][0]][0]-self.nodes[i][0],self.nodes[self.adj[i][j][0]][1]-self.nodes[i][1],length_includes_head = True, color = 'black')
                 for i in range(len(self.longpaths[p])-1):
-                    aux_ax.arrow(self.nodes[self.longpaths[p][i]][0],self.nodes[self.longpaths[p][i]][1],self.nodes[self.longpaths[p][i+1]][0]-self.nodes[self.longpaths[p][i]][0],self.nodes[self.longpaths[p][i+1]][1]-self.nodes[self.longpaths[p][i]][1], length_includes_head = True, color = 'red')
+                    plt.arrow(self.nodes[self.longpaths[p][i]][0],self.nodes[self.longpaths[p][i]][1],self.nodes[self.longpaths[p][i+1]][0]-self.nodes[self.longpaths[p][i]][0],self.nodes[self.longpaths[p][i+1]][1]-self.nodes[self.longpaths[p][i]][1],width=0.005,length_includes_head=True,color='darkorange')
                 for i in range(len(self.shortpaths[p])-1):
-                    aux_ax.arrow(self.nodes[self.shortpaths[p][i]][0],self.nodes[self.shortpaths[p][i]][1],self.nodes[self.shortpaths[p][i+1]][0]-self.nodes[self.shortpaths[p][i]][0],self.nodes[self.shortpaths[p][i+1]][1]-self.nodes[self.shortpaths[p][i]][1], length_includes_head = True, color = 'green')
+                    plt.arrow(self.nodes[self.shortpaths[p][i]][0],self.nodes[self.shortpaths[p][i]][1],self.nodes[self.shortpaths[p][i+1]][0]-self.nodes[self.shortpaths[p][i]][0],self.nodes[self.shortpaths[p][i+1]][1]-self.nodes[self.shortpaths[p][i]][1],width=0.005,length_includes_head=True,color='dodgerblue')
 
 #%%
-ps = np.linspace(0.75,1.25,num = 50)
-X = DAG(8,show=False)
-X.minkowski([0.5,1.5])
-X.short(findpath = True, displayps = [0.5,2])
-X.long(findpath = True, displayps = [0.5,2])
+ps = np.linspace(0.75,1.25,50)
+X = DAG(100)
+X.minkowski(ps)
+X.short(True,ps)
+X.long(True,ps)
 X.distance_comparison()
-X.show_all([0.5,2])
-
+X.show(ps)
